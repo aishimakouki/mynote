74e92b5b5ffa68b7dd2520d34be381aa1eee7304
kouki <miki.0418.kouki@gmail.com>
2018-05-31 09:54:07 +0900

    検索機能追加

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -3,7 +3,8 @@ class NotesController < ApplicationController
   def index
     @note = Note.new
     #昇順だったら　：asc
-    @notes = Note.new_order.page(params[:page])
+    @q = Note.new_order.ransack(params[:q])
+    @notes = @q.result.page(params[:page])
   end
   def create
     @note = Note.new(note_params)
